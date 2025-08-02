10f0a69879d89a88adfac6f9b4e6217eea9671ba
Eahtasham <eahtashamummam@gmail.com>
2025-08-02 18:23:36 +0530

    turns off eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      // Turn off specific rules here:
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
