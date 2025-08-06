637ee05136774e6d5cdd47ea071569a6131bfcae
Eahtasham <eahtashamummam@gmail.com>
2025-08-06 19:24:22 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-        remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
     domains: ['i.ibb.co', 'localhost', 'https://eahtasham-portfolio.vercel.app/'],
   },
 };
